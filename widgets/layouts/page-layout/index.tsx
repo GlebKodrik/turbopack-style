d76a76a4c8867ac8a3020ace9ab9e13bc37adbed
Gleb <kodrikgleb2@gmail.com>
2025-10-03 01:11:26 +0300

    Удалил стили

--- widgets/layouts/page-layout/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-type BasePageLayoutProps = {
-  children: ReactNode;
-};
-
-export const PageLayout = ({ children }: BasePageLayoutProps) => {
-  return <>{children}</>;
-};

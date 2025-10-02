ab0935b54a56b1b2a1fef839a9af2638f665968f
Gleb <kodrikgleb2@gmail.com>
2025-10-03 01:23:55 +0300

    Удалил стили

--- entities/course-program/index.ts
+++ entities/course-program/index.ts
@@ -1,4 +1,3 @@
 export { COURSE_TYPE, STATUSES } from './lib/constants';
-export { CourseProgramCarousel } from './ui/CourseProgramCarousel';
 export { CourseProgramLessonCard } from './ui/CourseProgramLessonCard';
 export { CourseProgramStudy } from './ui/CourseProgramStudy';

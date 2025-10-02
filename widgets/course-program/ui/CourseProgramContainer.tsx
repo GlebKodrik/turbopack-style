be0979bc0fa265d24ad7c5b8690ca060102ba546
Gleb <kodrikgleb2@gmail.com>
2025-10-03 00:56:35 +0300

    Удалил стили

--- widgets/course-program/ui/CourseProgramContainer.tsx
+++ widgets/course-program/ui/CourseProgramContainer.tsx
@@ -2,23 +2,12 @@ import React from 'react';
 
 import { CourseProgramStudy } from '@/entities/course-program';
 
-const course = {
-  children: [],
-  title: 'Test',
-  description_text: 'test',
-  image_absolute: 'test',
-  mentors: [],
-  program_description: 'Teset',
-};
-
 export const CourseProgramContainer = () => {
-  const { children, title, mentors } = course;
-
   return (
     <>
       <CourseProgramStudy
         image=""
-        title={title}
+        title="Hello"
         description=""
       />
     </>
--- widgets/intensives/ui/IntensivesContainer.tsx
+++ widgets/intensives/ui/IntensivesContainer.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 
 import { WhoItSuitableFor, YouWillFindOut } from '@/entities/intensives';
-import { AnimatedBlock } from '@/shared/ui/animated-block';
 
 import styles from './IntensivesContainer.module.scss';
 
@@ -15,16 +14,14 @@ export const IntensivesContainer = ({ uid }: IntensivesContainerProps) => {
       <div className="main-wrapper">
         <YouWillFindOut items={[{ text: 'dsfdff', image_absolute: '' }]} />
 
-        <AnimatedBlock className={styles.block}>
-          <WhoItSuitableFor
-            handleSubscribe={() => {}}
-            id={uid}
-            dayStart="22.06.2000"
-            forWhom={{ title: 'dsads', elements: [] }}
-            intensiveTitle="dsffdsf"
-            telegramBlock={{ title: 'Test', description: 'test' }}
-          />
-        </AnimatedBlock>
+        <WhoItSuitableFor
+          handleSubscribe={() => {}}
+          id={uid}
+          dayStart="22.06.2000"
+          forWhom={{ title: 'dsads', elements: [] }}
+          intensiveTitle="dsffdsf"
+          telegramBlock={{ title: 'Test', description: 'test' }}
+        />
       </div>
     </div>
   );
